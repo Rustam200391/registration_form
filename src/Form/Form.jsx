1cd91322a71949be57eceadffb5c50f25d175549
Rustam Guseinov <rg20031991.com>
2023-02-24 12:07:15 +0300

    intermedia commit

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -21,9 +21,13 @@ export function Form() {
 
                 <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
                     <input type="text" {...register("username")} placeholder='username' required />
+                    {/* register your input into the hook by invoking the "register" function */}
                     <input type="text" {...register("password")} placeholder='password' required />
+                    
                     <input type="text" {...register("confirmpwd")} placeholder='confirm password' />
-                    <input type="text" {...register("mobile", { required : true, maxLength: 11 })} placeholder='mobile number' />
+
+                    <input type="text" {...register("mobile", { required : true, maxLength: 11, })} placeholder='mobile number' />
+
                     {errors.mobile?.type === "required" && "Mobile Number is required"}
                     {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
                     <button className='btn' onSubmit>Sign In</button>
