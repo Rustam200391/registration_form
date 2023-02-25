8447ef9f1a8265241c469b5be5f0e3bfd320b812
Rustam Guseinov <rg20031991.com>
2023-02-25 11:54:31 +0300

    intermedia commit add title & button animation

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group'
 import { useForm } from 'react-hook-form';
 // import PhoneInput, { isValidPhoneNumber } from "react-phone-number-input";
-
 import './style.scss';
 
 
-export function Form(username,mobile) {
+export const Form =(username,mobile) => {
 
     const { register, handleSubmit, formState: { errors } } = useForm()
-    const onSubmit = data => console.log(data);
+    const [show,setShow] = useState(false)
+
+    const onSubmit = (data) => {console.log(data)};
      // console.log(entered data('username','password'...));
 
     const [value, setValue] = React.useState(() => localStorage.getItem(username,mobile) || '')
@@ -24,15 +26,24 @@ export function Form(username,mobile) {
                 <section>
                     <div className="register">
                         <div className="col-1">
-                            <h2>Sign In</h2>
-                            <span>register and enjoy the service</span>
+                            <h2 onClick={() => setShow(!show)}>Sign In</h2>
+                            {show ?'': 'enjoy the service'}
+                            
+                            <CSSTransition 
+                            in={show} 
+                            timeout={3000}
+                            classNames='alert'
+                            unmountOnExit
+                            >
+                            <h3>Registration process after filling out the form...</h3>
+                            </CSSTransition>
 
                             <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)} onChange={e => setValue(e.target.value)}>
                                 <input type="text" {...register("username")} placeholder='username' required />
                                 {/* зарегистрируйте свой ввод в hook, вызвав функцию "register" */}
                                 <input type="text" {...register("password")} placeholder='password' required  />
 
-                                <input type="text" {...register("confirmpwd")} placeholder='confirm password' />
+                                <input type="text" {...register("confirmpwd")} placeholder='confirm password'  />
                                 <input type="text" {...register("mobile", { required : true, maxLength: 11 ,  pattern: {
                                     value:
                                     /^(\+7)[\s(]*\d{3}[)\s]*\d{3}[\s-]?\d{2}[\s-]?\d{2}$/,
@@ -42,7 +53,14 @@ export function Form(username,mobile) {
 
                                 {errors.mobile?.type === "required" && "Mobile Number is required"}
                                 {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
-                                <button className='btn' onSubmit onClick={() => console.log(`${username}: ${localStorage.getItem(username)}, ${localStorage.getItem(mobile)}`)}>Sign In</button>
+                                <CSSTransition 
+                                    in={show} 
+                                    timeout={3000}
+                                    classNames='alert'
+                                    unmountOnExit
+                                    >
+                                <button className='btn' onSubmit={onSubmit} onClick={() => console.log(`${username}: ${localStorage.getItem(username)}, ${localStorage.getItem(mobile)}`)}>Sign In</button>
+                                </CSSTransition>
                             </form>
                             
                         </div>
