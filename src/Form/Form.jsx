c8521eb6fc5313cb2c2f553c3526e46de8eab834
Rustam Guseinov <rg20031991.com>
2023-02-25 16:01:57 +0300

    intermedia commit

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -5,20 +5,20 @@ import { useForm } from 'react-hook-form';
 import './style.scss';
 
 
-export const Form =(username,mobile) => {
+export const Form =(name,mobile) => {
 
     const { register, handleSubmit, formState: { errors } } = useForm()
-    const [show,setShow] = useState(false)
+    const [show,setShow] = useState(true)
 
     const onSubmit = (data) => {console.log(data)};
      // console.log(entered data('username','password'...));
 
-    const [value, setValue] = React.useState(() => localStorage.getItem(username,mobile) || '')
-    //нужно что бы и mobile сохранился в localstorage
+    const [value, setValue] = React.useState(() => localStorage.getItem(name) || '')
+    //нужно что бы все поля сохранились в localstorage
     
         React.useEffect(() => {
-             localStorage.setItem(username,mobile, value)
-            }, [username,mobile, value])
+             localStorage.setItem(name, value)
+            }, [name, value])
 
    
     
@@ -27,7 +27,7 @@ export const Form =(username,mobile) => {
                     <div className="register">
                         <div className="col-1">
                             <h2 onClick={() => setShow(!show)}>Sign In</h2>
-                            {show ?'': 'enjoy the service'}
+                            {show ?'Enjoy': 'The service'}
                             
                             <CSSTransition 
                             in={show} 
@@ -49,18 +49,18 @@ export const Form =(username,mobile) => {
                                     /^(\+7)[\s(]*\d{3}[)\s]*\d{3}[\s-]?\d{2}[\s-]?\d{2}$/,
                                     // регулярное выражение при попытке ввода 8 и 7 цифра меняется на +7, так же что бы номер автоматически прописывался а вскобках и с разделением знаком " - "
                                     message: "Email must be valid",
-                                }, })} placeholder='mobile number' />
+                                }, })} placeholder='mobile number' pattern="-?\d{1,3}\.\d+"/>
 
                                 {errors.mobile?.type === "required" && "Mobile Number is required"}
                                 {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
-                                <CSSTransition 
+                                {/* <CSSTransition 
                                     in={show} 
                                     timeout={3000}
                                     classNames='alert'
                                     unmountOnExit
-                                    >
-                                <button className='btn' onSubmit={onSubmit} onClick={() => console.log(`${username}: ${localStorage.getItem(username)}, ${localStorage.getItem(mobile)}`)}>Sign In</button>
-                                </CSSTransition>
+                                    > */}
+                                <button className='btn' onSubmit={onSubmit} onClick={() => console.log(`${name}: ${localStorage.getItem(name)}, ${localStorage.getItem(mobile)}`)}>Sign In</button>
+                                {/* </CSSTransition> */}
                             </form>
                             
                         </div>
